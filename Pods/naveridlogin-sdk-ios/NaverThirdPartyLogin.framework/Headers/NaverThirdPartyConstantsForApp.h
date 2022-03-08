6f9aa28c283cde104ec733728057b5d54c8f4728
doyun <doyun>
2022-03-08 22:14:29 +0900

    :bug: fix:  프로젝트 설정 변경
           body: 맥북 변경으로 인한 프로젝트 환경 변경

--- Pods/naveridlogin-sdk-ios/NaverThirdPartyLogin.framework/Headers/NaverThirdPartyConstantsForApp.h
+++ Pods/naveridlogin-sdk-ios/NaverThirdPartyLogin.framework/Headers/NaverThirdPartyConstantsForApp.h
@@ -32,8 +32,8 @@ typedef enum {
     OK
 } OAuthLoginState;
 
-#define kServiceAppUrlScheme    @"withpet_ios"
+#define kServiceAppUrlScheme    @"thirdparty20samplegame"
 
-#define kConsumerKey            @"uSlqej8yd9DCXYGbNuqw"
-#define kConsumerSecret         @"APqAHgYNLJ"
-#define kServiceAppName         @"withpet_iOS"
+#define kConsumerKey            @"jyvqXeaVOVmV"
+#define kConsumerSecret         @"527300A0_COq1_XV33cf"
+#define kServiceAppName         @"네이버 아이디로 로그인"
